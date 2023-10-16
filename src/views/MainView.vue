8d6bdb7c2d50fb5a161552b09b216e9dff3b6891
bohub12 <qhrb1997@naver.com>
2023-10-17 02:55:02 +0900

    Feat: 사이드바 컴포넌트

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,15 +1,33 @@
 <template>
   <HeaderComponent />
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary"
+      >
+        <div
+          class="offcanvas-md offcanvas-end bg-body-tertiary"
+          tabindex="-1"
+          id="sidebarMenu"
+          aria-labelledby="sidebarMenuLabel"
+        >
+          <SidebarComponent />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderComponent from "@/components/HeaderComponent.vue"; // @ is an alias to /src
+import SidebarComponent from "@/components/SidebarComponent.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     HeaderComponent,
+    SidebarComponent,
   },
 });
 </script>
