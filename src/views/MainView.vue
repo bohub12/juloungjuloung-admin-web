820bb3fe75aa9e1f3879490d3162bb30abe5dacf
bohub12 <qhrb1997@naver.com>
2023-10-17 02:13:23 +0900

    Feat: 헤더 컴포넌트 구현

---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+  <HeaderComponent />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderComponent from "@/components/HeaderComponent.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    HeaderComponent,
+  },
+});
+</script>
