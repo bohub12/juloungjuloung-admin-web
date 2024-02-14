a4a369e365fab36e66c76d6d4feb4aa6a2128f3e
bohub12 <qhrb1997@naver.com>
2024-02-14 16:45:24 +0900

    Feat: product category - vuex state

---
+++ src/store/product/productCategory.ts
@@ -0,0 +1,37 @@
+interface ProductCategory {
+  name: string;
+  value: string;
+}
+
+interface ProductCategoryState {
+  productCategories: ProductCategory[];
+  currentProductCategory: string;
+}
+
+export default {
+  namespaced: true,
+  state: {
+    productCategories: [
+      { name: "반지", value: "RING" },
+      { name: "목걸이", value: "NECKLACE" },
+      { name: "귀걸이", value: "EARRING" },
+      { name: "팔찌", value: "BRACELET" },
+    ],
+    currentProductCategory: null,
+  },
+  mutations: {
+    setProductCategory(
+      state: ProductCategoryState,
+      newProductCategory: string
+    ) {
+      state.currentProductCategory = newProductCategory;
+    },
+  },
+  actions: {
+    // 다른 액션들을 추가할 수 있음
+  },
+  getters: {
+    productCategories: (state: ProductCategoryState) => state.productCategories,
+    currentTheme: (state: ProductCategoryState) => state.currentProductCategory,
+  },
+};
