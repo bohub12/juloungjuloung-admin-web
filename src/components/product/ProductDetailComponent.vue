82d7595479115fd5bf1a3b0d4b01bd6c7bdcf993
bohub12 <qhrb1997@naver.com>
2024-03-01 02:05:28 +0900

    Feature: 상품 상세 컴포넌트 모드별로 분리

--- src/components/product/ProductDetailComponent.vue
+++ src/components/product/ProductDetailComponent.vue
@@ -1,127 +1,119 @@
 <template>
   <div class="container mt-5">
-    <h2 v-if="mode === 'view'">Product Detail</h2>
-    <h2 v-else-if="mode === 'edit'">Add Product</h2>
-    <h2 v-else>Edit Product</h2>
-    <form @submit.prevent="handleSubmit">
-      <div class="mb-3">
-        <label for="productName" class="form-label">상품 이름</label>
+    <h2>Product Detail</h2>
+    <div class="mb-3">
+      <label for="productName" class="form-label">상품 이름</label>
+      <input
+        type="text"
+        class="form-control"
+        id="productName"
+        v-model="product.name"
+        required
+        readonly
+      />
+    </div>
+    <div class="mb-3">
+      <div>상품 전시여부</div>
+      <div class="form-check form-switch">
         <input
-          type="text"
-          class="form-control"
-          id="productName"
-          v-model="product.name"
-          required
-          readonly
+          class="form-check-input"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckChecked"
+          checked
+          disabled
         />
       </div>
-      <div class="mb-3">
-        <div>상품 전시여부</div>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckChecked"
-            checked
-            disabled
-          />
-        </div>
-        <!-- <label class="form-check-label" for="flexSwitchCheckChecked"
+      <!-- <label class="form-check-label" for="flexSwitchCheckChecked"
           >상품 전시여부
         </label> -->
-      </div>
+    </div>
 
-      <div class="mb-3">
-        <label for="productType" class="form-label">상품 타입</label>
-        <input
-          type="text"
-          class="form-control"
-          id="productType"
-          v-model="product.type"
-          disabled
-          readonly
+    <div class="mb-3">
+      <label for="productType" class="form-label">상품 타입</label>
+      <input
+        type="text"
+        class="form-control"
+        id="productType"
+        v-model="product.type"
+        disabled
+        readonly
+      />
+    </div>
+
+    <div class="mb-3">
+      <label for="productCode" class="form-label">상품 코드</label>
+      <input
+        type="text"
+        class="form-control"
+        id="productCode"
+        v-model="product.code"
+        disabled
+        readonly
+      />
+    </div>
+
+    <div class="mb-3">
+      <label for="productPrice" class="form-label">상품 가격(원)</label>
+      <input
+        type="text"
+        class="form-control"
+        id="productPrice"
+        v-model="product.price"
+        required
+        readonly
+      />
+    </div>
+    <div class="mb-3">
+      <label for="productWeight" class="form-label">상품 무게(g)</label>
+      <input
+        type="text"
+        class="form-control"
+        id="productWeight"
+        v-model="product.weight"
+        required
+        readonly
+      />
+    </div>
+    <div class="mb-3">
+      <div>상품 색상</div>
+      <ul class="list-group list-group-horizontal">
+        <li
+          v-for="color in product.colors"
+          :key="color.color"
+          class="list-group-item"
+        >
+          {{ color.color }} (+{{ color.additionalPrice }}원)
+        </li>
+      </ul>
+    </div>
+
+    <div class="mb-3">
+      <div>상품 재질</div>
+      <ul class="list-group list-group-horizontal">
+        <li
+          v-for="material in product.materials"
+          :key="material.material"
+          class="list-group-item"
+        >
+          {{ material.material }} (+{{ material.additionalPrice }}원)
+        </li>
+      </ul>
+    </div>
+
+    <div class="mb-3">
+      <div class="mt-3">상품 이미지</div>
+      <ul class="list-group list-group-horizontal">
+        <img
+          v-for="image in product.images"
+          :key="image.imageUrl"
+          :src="image.imageUrl"
+          width="200"
+          height="200"
+          class="rounded ml-1"
         />
-      </div>
-
-      <div class="mb-3">
-        <label for="productCode" class="form-label">상품 코드</label>
-        <input
-          type="text"
-          class="form-control"
-          id="productCode"
-          v-model="product.code"
-          disabled
-          readonly
-        />
-      </div>
-
-      <div class="mb-3">
-        <label for="productPrice" class="form-label">상품 가격(원)</label>
-        <input
-          type="text"
-          class="form-control"
-          id="productPrice"
-          v-model="product.price"
-          required
-          readonly
-        />
-      </div>
-      <div class="mb-3">
-        <label for="productWeight" class="form-label">상품 무게(g)</label>
-        <input
-          type="text"
-          class="form-control"
-          id="productWeight"
-          v-model="product.weight"
-          required
-          readonly
-        />
-      </div>
-      <div class="mb-3">
-        <div>상품 색상</div>
-        <ul class="list-group list-group-horizontal">
-          <li
-            v-for="color in product.colors"
-            :key="color.color"
-            class="list-group-item"
-          >
-            {{ color.color }} (+{{ color.additionalPrice }}원)
-          </li>
-        </ul>
-      </div>
-
-      <div class="mb-3">
-        <div>상품 재질</div>
-        <ul class="list-group list-group-horizontal">
-          <li
-            v-for="material in product.materials"
-            :key="material.material"
-            class="list-group-item"
-          >
-            {{ material.material }} (+{{ material.additionalPrice }}원)
-          </li>
-        </ul>
-      </div>
-
-      <div class="mb-3">
-        <div class="mt-3">상품 이미지</div>
-        <ul class="list-group list-group-horizontal">
-          <img
-            v-for="image in product.images"
-            :key="image.imageUrl"
-            :src="image.imageUrl"
-            width="200"
-            height="200"
-            class="rounded ml-1"
-          />
-        </ul>
-      </div>
-
-      <button v-if="mode !== 'view'" type="submit" class="btn btn-primary">
-        {{ mode === "create" ? "Add Product" : "Save Changes" }}
-      </button>
-    </form>
+      </ul>
+    </div>
   </div>
 </template>
 
