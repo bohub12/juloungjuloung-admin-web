792dd31940baa5880b2cbed52d2619a34ffc0668
bohub12 <qhrb1997@naver.com>
2024-03-23 20:34:07 +0900

    Feat: 상품 이미지 업로드 버튼 및 로직 구현

--- src/components/product/SaveProductDetailComponent.vue
+++ src/components/product/SaveProductDetailComponent.vue
@@ -58,10 +58,28 @@
       </div>
 
       <div class="mb-3">
-        <div class="mt-3">상품 이미지</div>
-        <ul class="list-group list-group-horizontal">
+        <div class="d-flex justify-content-between">
+          <div>상품 이미지</div>
+          <label for="file-upload" class="custom-file-upload bg-primary badge">
+            <!-- <span class="fs-2"> -->
+            <i
+              class="bi bi-cloud-arrow-up-fill"
+              style="padding-right: 0.25rem"
+            ></i>
+            <!-- </span> -->
+            <span>상품 이미지 업로드</span>
+          </label>
+          <input
+            id="file-upload"
+            type="file"
+            accept=".jpg, .jpeg, .png"
+            @change="imageFileUploadToS3"
+          />
+        </div>
+        <ul class="list-group list-group-horizontal overflow-auto">
           <img
-            v-for="image in product.images"
+            v-for="image in this.productImages
+              .upsertProductImageInternalRequests"
             :key="image.imageUrl"
             :src="image.imageUrl"
             width="200"
@@ -114,6 +132,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useStore } from "vuex";
+import axios from "axios";
 import {
   DefaultApiFactory,
   SaveProductRequest,
@@ -128,6 +147,7 @@ export default defineComponent({
   name: "SaveProductDetailComponent",
   data() {
     return {
+      savedProductId: 0 as number,
       availableProductTypes: [
         SaveProductRequestProductTypeEnum.Bracelet,
         SaveProductRequestProductTypeEnum.Earring,
@@ -136,6 +156,7 @@ export default defineComponent({
       ] as Array<SaveProductRequestProductTypeEnum>,
       product: {} as SaveProductRequest,
       productImages: {} as UpsertProductImageRequest,
+      productImageFiles: [] as Array<File>,
       productOptionInfos: {} as UpsertProductOptionRequest,
     };
   },
@@ -146,12 +167,95 @@ export default defineComponent({
   methods: {
     handleSubmit() {
       this.saveProduct();
-      console.log("Form Saved");
+
+      this.productImages.productId = this.savedProductId;
+      this.productOptionInfos.productId = this.savedProductId;
+
+      this.saveProductImages();
+      this.saveProductOptionInfos();
+    },
+    async saveProduct() {
+      defaultApi
+        .saveProduct(this.product)
+        .then((res) => {
+          this.savedProductId = res.data.data!;
+        })
+        .catch((error) => console.error(error));
+    },
+    async saveProductImages() {
+      for (const productImageFile of this.productImageFiles) {
+        this.saveProductImage(productImageFile);
+      }
+    },
+    async saveProductImage(imageFile: File) {
+      // save product image info to my server
+    },
+    async imageFileUploadToS3(event: Event) {
+      const input = event.target as HTMLInputElement;
+      if (input.files) {
+        const file = input.files[0];
+        const resposne = await defaultApi.createPreSignedUrl();
+
+        const preSignedUrl = resposne.data.data!.preSignedUrl;
+        const virtualImagePath = resposne.data.data!.virtualImagePath;
+
+        await this.uploadImageToS3(preSignedUrl, file);
+
+        if (!this.productImages.upsertProductImageInternalRequests) {
+          this.productImages.upsertProductImageInternalRequests = [
+            {
+              id: 0,
+              imageUrl: virtualImagePath,
+              isThumbnail: false,
+            },
+          ];
+        } else {
+          this.productImages.upsertProductImageInternalRequests.push({
+            id: 0,
+            imageUrl: virtualImagePath,
+            isThumbnail: false,
+          });
+        }
+
+        console.log(virtualImagePath);
+      }
+    },
+    async uploadImageToS3(preSignedUrl: string, imageFile: File) {
+      await axios
+        .put(preSignedUrl, imageFile, {
+          headers: {
+            "Content-Type": "image/jpg",
+          },
+        })
+        .then((res) => console.log(res))
+        .catch((error) => console.error(error));
     },
 
-    async saveProduct() {
-      const response = defaultApi.saveProduct(this.product);
+    async saveProductOptionInfos() {
+      // save product option infos
+
+      console.log("save product option infos");
     },
   },
 });
 </script>
+
+<style scoped>
+input[type="file"] {
+  display: none;
+}
+.custom-file-upload {
+  border-radius: 0.5rem;
+  display: inline-block;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.75rem;
+}
+/* 
+.custom-file-upload span {
+  display: block;
+  text-align: center;
+} */
+</style>
